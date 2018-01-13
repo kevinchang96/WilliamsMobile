0019d291aa1fcafa2616075514a8be0b33241a7c
Dysron <dysronm@gmail.com.com>
2018-01-13 00:20:39 -0500

    Factrak autocomplete | created document from search | navigation and parsing left

--- App/Components/FactrakSearch.js
+++ App/Components/FactrakSearch.js
@@ -10,62 +10,95 @@ import {
     ImageBackground
     } from 'react-native';
 import SuggestionCard from './SuggestionCard';
-import Cookie from 'react-native-cookie';
 
 export default class FactrakSearch extends Component{
     constructor(props){
         super();
-        this.state = {jArr:[],suggestions:[]};
+        this.state = {  jArr:[],
+                        suggestions:[],
+                        currentHTMLDoc:"",
+                        currentJSON:{}  // for use if the selected suggestion is a professor
+                    };
+    }
+    selected = (type,title,id) => {
+        const dir = (type == "name") ? "professors/" : "courses/";
+        const url = "https://wso.williams.edu/factrak/" + dir + id;
+
+        fetch(url,         // NodeList after assignment
+                {
+                credentials: 'include',
+                mode: "no-cors",
+                method: "GET",
+                headers: {
+                    "Accept": "text/html"
+                }
+                }).then(function(response) {
+                    if(response.status == 200) return response.text();
+                }).then(function(text){
+                    const DOMParser = require('react-native-html-parser').DOMParser;
+                    let doc = new DOMParser().parseFromString(text,'text/html');
+                    this.setState({currentHTMLDoc:doc});
+                }.bind(this));
+
+        if(this.state.html) return;     // if unsuccessful response, do not continue
+
+        fetch(url,
+            {
+            credentials: 'include',
+            mode: "no-cors",
+            method: "GET",
+            headers: {
+                "Accept": "application/json"
+            }
+            }).then(function(response) {
+                if(response.status == 200) return response.json();
+            }).then(function(jsonResponse){
+                this.setState({currentJSON:jsonResponse});
+            }.bind(this));
+
+            console.log(this);
     }
 
     getSuggestions(text){
-        /*Cookie.get("https://wso.williams.edu/",
-            '_WSOonRails_session').then((cookie) => console.log(cookie));
-        */
-
-        Cookie.set("https://wso.williams.edu",'_WSOonRails_session',this.state.cookies,{
-            path: "/factrak/autocomplete.json"+text,
-            domain: 'wso.williams.edu'
-        }).then(() => console.log('success'));
-
        fetch("https://wso.williams.edu/factrak/autocomplete.json?q=" + text,
            {
             credentials: 'include',
             mode: "no-cors",
             method: "GET",
             headers: {
-                "Accept": "application/json"
+                "Accept": "application/json",
                 }
             }).then(function(response) {
-                console.log(response);
+                //console.log(response);
                 return response.json();
             }).then(function(jsonResponse){
-                console.log(jsonResponse);
-                this.state.jArr = (jsonResponse) ? jsonResponse : "";
-            }).catch(function(error) {
-                console.log('There has been a problem with your fetch operation: ', error.message);
-            });
+                this.setState({jArr:jsonResponse});
+                console.log(this.state.jArr);
+            }.bind(this));
     }
-    createSuggestionBoxes(){
+
+    createSuggestionBoxes(selected){
         this.state.suggestions = this.state.jArr.map(   // array of suggestion cards
-            function(current){
-                console.log(current);
-                key = Object.keys(current)[0];
+            function(current,i){
+                //console.log(current);
+                let type = Object.keys(current)[0];
                 return(<SuggestionCard id={current['id']}
-                        title={current['name'] || current['title']} type={key}/>);
+                        title={current['name'] || current['title']} type={type} key={i}
+                        selected={selected}/>);
             }
-        );
+        )
+        return this.state.suggestions;
     }
     render(){
         return(
             <View style={styles.container}>
                 <View style={styles.searchBox}>
-                    <TextInput onChange={console.log(this.createSuggestionBoxes())}
+                    <TextInput onChangeText={(text) => this.getSuggestions(text)}
                     placeholder="Search for a professor or course..." />
                 </View>
                 <View style={styles.suggestionsContainer}>
-                    <Text>There are: {this.state.jArr.length} items to be displayed</Text>
-                    {/*console.log(this.state.suggestions)*/}
+                    {/*<Text>There are: {this.state.jArr.length} items to be displayed</Text>*/}
+                    {this.createSuggestionBoxes(this.selected)}
                 </View>
             </View>
         );
--- App/Components/SuggestionCard.js
+++ App/Components/SuggestionCard.js
@@ -6,48 +6,18 @@ import {
     Text,
     View,
     TouchableOpacity,
-    } from 'react-native';
+} from 'react-native';
 
 export default class SuggestionCard extends Component{
-    // this.props.id
-    // this.props.title
+
+    selected = () => {
+        this.props.selected(this.props.type,this.props.title,this.props.id);
+    }
 
     render(){
-        const dir = (this.props.type == "name") ? "professors/" : "courses/";
-        const url = "https://wso.williams.edu/factrak/" + dir + this.props.id;
-        let html;           // NodeList after assignment
-        fetch(url,
-            {
-            credentials: 'same-origin',
-            mode: "no-cors",
-            method: "GET",
-            headers: {
-                "Accept": "text/html"
-            }
-            }).then(function(response) {
-                return response.text();
-            }).then(function(text){
-                html = text;
-            });
-        let json;           // for use if the selected suggestion is a professor
-        fetch(url,
-            {
-            credentials: 'same-origin',
-            mode: "no-cors",
-            method: "GET",
-            headers: {
-                "Accept": "application/json"
-            }
-            }).then(function(response) {
-
-                return response.json();
-            }).then(function(jsonResponse){
-                if(jsonResponse.status) json = null;    // if we receive a json with status error
-                else json = jsonResponse;
-            });
         return(
-            <TouchableOpacity style={styles.container} onPress={this.props.onPress} html={html}
-                json={json}>
+            <TouchableOpacity style={styles.container}
+                onPress={this.selected}>
                 <Text>{this.props.title}</Text>
                 {/* onPress will remove suggestions and pass props to comment window */}
             </TouchableOpacity>
