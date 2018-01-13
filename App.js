603d909c5935ecd1e0d35289d1ea0a2d59271db4
Dysron <dysronm@gmail.com.com>
2018-01-13 00:22:27 -0500

    Factrak autocomplete | created document from search | navigation and parsing left

--- App.js
+++ App.js
@@ -1,30 +1,40 @@
 /**
- * Kevin Chang, David Ariyibi, Dysron Marshall
- * (c) 12/2017, 01/2018
+ * Kevin Chang
+ * (c) 2017
  */
 
 import React, { Component } from 'react';
 import { AppRegistry, Platform, StyleSheet, Text, View } from 'react-native';
 import Login from './App/Components/Login';
-import Logout from './App/Components/Logout';
+import Logout from './App/Components/Logout'
 import Component2 from './App/Components/Component2';
 import Settings from './App/Components/Settings';
 import HttpExample from './App/Components/HttpExample';
-//import Home from './App/Containers/Home'
+import {FactrakComment} from './App/Components/FactrakCommentWindow';
+import FactrakSearch from './App/Components/FactrakSearch';
+import SuggestionCard from './App/Components/SuggestionCard';
+
 
 export default class main extends Component{
+
+    componentDidUpdate(){
+        console.log("updated");
+    }
+
     render(){
         return(
-            <View backgroundColor={"#512698"} >
+            <View style={{paddingTop:20}}>
                 <Login />
                 <Logout />
-//                <Home />
-                <Component2 />
-                <Settings />
-
+                <FactrakSearch />
+                {/*<FactrakComment professorName="Stewart Johnson" courseNo="MATH 150" numAgree="10"
+                     numDisagree="5" responseComponents={[<Text key={1}>Good professor.</Text>,
+                                                            <Text key={2}>Would take again.</Text>]}
+                     takeAgain="I would" wouldRecommend="I wouldn't"
+                     postedWhen="2 months ago"/>*/}
             </View>
+
         );
     }
 }
-
- AppRegistry.registerComponent('main', () => main );
+AppRegistry.registerComponent('main', () => main );
