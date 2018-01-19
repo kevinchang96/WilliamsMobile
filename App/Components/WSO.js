dca7cf4dbf5d8c917f00fa450d62d9f639886c99
Kevin Chang <kcha1224@gmail.com>
2018-01-19 17:57:35 -0500

    Preliminary version of WSO implemented. Need to clean code, but now WSO is working.

--- App/Components/WSO.js
+++ App/Components/WSO.js
@@ -29,6 +29,10 @@ export default class WSO extends Component{
         };
     }
 
+    componentDidMount(){
+        this.parseWSO();
+    }
+
     parseWSO(){
         var html = '';
         fetch('https://wso.williams.edu/', {method: 'GET'})
@@ -42,8 +46,94 @@ export default class WSO extends Component{
             let doc = new DOMParser({errorHandler:{warning:function(w){console.warn(w)},error:function(w){console.log(w)},fatalError:function(w){console.log(w)}}}).parseFromString(html,'text/html');
             //console.log(doc);
             var input = doc.getElementsByTagName("section");
-            console.log( "Size: " + input.length );
-            console.log( "Child: " + input.item(1) );
+            //console.log( "Size: " + input.length );
+            const links = input.item(0).getElementsByTagName("a");
+            var size = links.length;
+            //console.log(links.item(0));
+            console.log( "Child: " + links );
+            console.log( "First child: " + links.item(0) );
+            var temp = [5];
+            for( var i = 1; i < 6; i++ ){
+                var discussions = {
+                    link: links.item(i).attributes.item(0).value,
+                    text: links.item(i).textContent
+                };
+                temp[i-1] = discussions;
+                //temp.push(discussions);
+            };
+            //this.setState( {discussions: temp} );
+            var temp1 = [5];
+            for( var j = 7; j < 12; j++ ){
+                var announcements = {
+                    link: links.item(j).attributes.item(0).value,
+                    text: links.item(j).textContent
+                };
+                temp1[j-7] = announcements;
+                //temp1.push(announcements);
+            };
+            //this.setState( {announcements: temp} );
+            var temp2 = [5];
+            for( var k = 13; k < 18; k++ ){
+                var exchanges = {
+                    link: links.item(k).attributes.item(0).value,
+                    text: links.item(k).textContent
+                };
+                temp2[k-13] = exchanges;
+                //temp2.push(exchanges);
+            };
+            //this.setState( {exchanges: temp} );
+            var temp3 = [5];
+            for( var l = 19; l < 24; l++ ){
+                var lostNfound = {
+                    link: links.item(l).attributes.item(0).value,
+                    text: links.item(l).textContent
+                };
+                temp3[l-19] = lostNfound;
+                //temp3.push(lostNfound);
+            };
+            //this.setState( {lostNfound: temp} );
+            var temp4 = [5];
+            for( var m = 25; m < 30; m++ ){
+                var jobs = {
+                    link: links.item(m).attributes.item(0).value,
+                    text: links.item(m).textContent
+                };
+                temp4[m-25] = jobs;
+                //temp4.push(jobs);
+            };
+            //this.setState( {jobs: temp} );
+            let temp5 = [5];
+            for( var n = 31; n < 36; n++ ){
+                var rides = {
+                    link: links.item(n).attributes.item(0).value,
+                    text: links.item(n).textContent
+                };
+                temp5[n-31] = rides;
+                //temp5.push(rides);
+            };
+            //this.setState( {rides: temp} );
+
+            //console.log("More children: " + links.item(i));
+            //href=""
+            //console.log(""+links.item(i).attributes.item(0) );
+            //link
+            //console.log(""+links.item(i).attributes.item(0).value );
+
+            //textContent
+            //console.log(""+links.item(i).textContent );
+
+            //this.setState({discussions:input.item(0).getElementsByTagName("a").childNodes});
+            //console.log( this.state );
+            this.setState(
+                        {
+                            discussions: temp,
+                            announcements: temp1,
+                            exchanges: temp2,
+                            lostNfound: temp3,
+                            jobs: temp4,
+                            rides: temp5
+                        }
+                        );
         }
         )
         .catch((error) => {
@@ -52,18 +142,20 @@ export default class WSO extends Component{
     }
 
     render(){
-        this.parseWSO();
+        //this.parseWSO();
+        //console.log("hello");
+        console.log(this.state);
         return(
          <View >
          <ScrollView>
             <Card title='Discussions'
                 containerStyle={{padding: 10}}>
               {
-                this.state.users.map((u, i) => {
+                this.state.discussions.map((u, i) => {
                     return(
                     <ListItem
                         key={i}
-                        title={u.name}
+                        title={u.text}
                         hideChevron={true}
                      />
                     );
@@ -77,11 +169,11 @@ export default class WSO extends Component{
             <Card title='Announcements'
                 containerStyle={{padding: 10}}>
               {
-                this.state.users.map((u, i) => {
+                this.state.announcements.map((u, i) => {
                     return(
                     <ListItem
                         key={i}
-                        title={u.name}
+                        title={u.text}
                         hideChevron={true}
                      />
                     );
@@ -95,11 +187,11 @@ export default class WSO extends Component{
             <Card title='Exchanges'
                 containerStyle={{padding: 10}}>
               {
-                this.state.users.map((u, i) => {
+                this.state.exchanges.map((u, i) => {
                     return(
                     <ListItem
                         key={i}
-                        title={u.name}
+                        title={u.text}
                         hideChevron={true}
                      />
                     );
@@ -113,11 +205,11 @@ export default class WSO extends Component{
             <Card title='Lost & Found'
                 containerStyle={{padding: 10}}>
               {
-                this.state.users.map((u, i) => {
+                this.state.lostNfound.map((u, i) => {
                     return(
                     <ListItem
                         key={i}
-                        title={u.name}
+                        title={u.text}
                         hideChevron={true}
                      />
                     );
@@ -131,11 +223,11 @@ export default class WSO extends Component{
             <Card title='Jobs'
                 containerStyle={{padding: 10}}>
               {
-                this.state.users.map((u, i) => {
+                this.state.jobs.map((u, i) => {
                     return(
                     <ListItem
                         key={i}
-                        title={u.name}
+                        title={u.text}
                         hideChevron={true}
                      />
                     );
@@ -149,11 +241,11 @@ export default class WSO extends Component{
             <Card title='Rides'
                 containerStyle={{padding: 10}}>
               {
-                this.state.users.map((u, i) => {
+                this.state.rides.map((u, i) => {
                     return(
                     <ListItem
                         key={i}
-                        title={u.name}
+                        title={u.text}
                         hideChevron={true}
                      />
                     );
